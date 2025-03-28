05ae7929e0d8a1ca0aa7debb1a15d03f1eb6d4fe
SamyakJain1991 <samyakjain15@gmail.com>
2025-03-28 22:21:40 +0530

    Update upload-photo.js

--- netlify/functions/upload-photo.js
+++ netlify/functions/upload-photo.js
@@ -1,3 +1,7 @@
+console.log('SUPABASE_URL:',
+process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:',
+process.env.SUPABASE_KEY);
 const { createClient } = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
