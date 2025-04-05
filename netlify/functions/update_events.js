e8257862da41cf7507944c6b9d0e506bd165fa2b
SamyakJain1991 <samyakjain15@gmail.com>
2025-04-05 10:30:51 +0530

    Update update_events.js

--- netlify/functions/update_events.js
+++ netlify/functions/update_events.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async function (event, context) {
   const githubToken = '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';
   const repoOwner = 'SamyakJain1991';
@@ -15,9 +13,31 @@ exports.handler = async function (event, context) {
       }
     }
   );
+  const getData = await getResponse.json();
+  const currentContent = Buffer.from(getData.content, 'base64').toString('utf8');
+  const events = JSON.parse(currentContent);
+
+  const newEvent = JSON.parse(event.body);
+  events.push(newEvent);
+
+  const updateResponse = await fetch(
+    https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath},
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: token ${githubToken},
+        Accept: 'application/vnd.github.v3+json'
+      },
+      body: JSON.stringify({
+        message: 'Update events.json',
+        content: Buffer.from(JSON.stringify(events)).toString('base64'),
+        sha: getData.sha
+      })
+    }
+  );
 
   return {
     statusCode: 200,
-    body: 'Fetch Call Done'
+    body: 'Event Updated Successfully!'
   };
 };
