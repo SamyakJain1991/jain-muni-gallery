402609df3cfda21df87193f92188dac2b952d57f
SamyakJain1991 <samyakjain15@gmail.com>
2025-04-05 11:22:31 +0530

    Update update_events.js

--- netlify/functions/update_events.js
+++ netlify/functions/update_events.js
@@ -1,43 +1,8 @@
 exports.handler = async function (event, context) {
-  const githubToken = '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';
-  const repoOwner = 'SamyakJain1991';
-  const repoName = 'jain-muni-gallery';
-  const filePath = 'events.json';
-
-  const getResponse = await fetch(
-    https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath},
-    {
-      headers: {
-        Authorization: token ${githubToken},
-        Accept: 'application/vnd.github.v3+json'
-      }
-    }
-  );
-  const getData = await getResponse.json();
-  const currentContent = Buffer.from(getData.content, 'base64').toString('utf8');
-  const events = JSON.parse(currentContent);
-
-  const newEvent = JSON.parse(event.body);
-  events.push(newEvent);
-
-  const updateResponse = await fetch(
-    https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath},
-    {
-      method: 'PUT',
-      headers: {
-        Authorization: token ${githubToken},
-        Accept: 'application/vnd.github.v3+json'
-      },
-      body: JSON.stringify({
-        message: 'Update events.json',
-        content: Buffer.from(JSON.stringify(events)).toString('base64'),
-        sha: getData.sha
-      })
-    }
-  );
+  const response = await fetch('https://api.github.com');
 
   return {
     statusCode: 200,
-    body: 'Event Updated Successfully!'
+    body: 'Minimal Fetch Done'
   };
 };
