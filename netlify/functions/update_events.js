a4e2eb71a5dcfdacdbf7dd91b92b735cbdf3c843
SamyakJain1991 <samyakjain15@gmail.com>
2025-04-05 11:44:11 +0530

    Update update_events.js

--- netlify/functions/update_events.js
+++ netlify/functions/update_events.js
@@ -1,15 +1,10 @@
 exports.handler = async function (event, context) {
-  const githubToken = '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';
-  const repoOwner = 'SamyakJain1991';
-  const repoName = 'jain-muni-gallery';
-  const filePath = 'events.json';
-
   const response = await fetch(
-    https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}
+    'https://api.github.com/repos/SamyakJain1991/jain-muni-gallery/contents/events.json'
   );
 
   return {
     statusCode: 200,
-    body: 'Fetch with URL Done'
+    body: 'Fetch with Hardcoded URL Done'
   };
 };
